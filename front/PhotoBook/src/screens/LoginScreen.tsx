68d5f1f08fa4f065a067b6bde53093b66c522dc1
Killian TERROM <killian.terrom@ynov.com>
2022-06-08 17:29:42 +0200

    jme souviens c'est le dernier commit de la journée

--- front/PhotoBook/src/screens/LoginScreen.tsx
+++ front/PhotoBook/src/screens/LoginScreen.tsx
@@ -1,13 +1,29 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { SafeAreaInsetsContext } from "react-native-safe-area-context";
 import { RootStackParamList } from "../navigation";
 
 type LoginProps = NativeStackScreenProps<RootStackParamList, 'Login' >;
 const LoginScreen = ({ navigation }: LoginProps) => {
     return (
         <View style={styles.mainContainer}>
-        <Text style={styles.loginscreen}> Login Screen</Text>
+        <Text style={styles.loginscreen}> Connexion</Text>
+        <TextInput
+        style={styles.textInput}
+        onChangeText={newText => {}}
+        defaultValue={""}
+        placeholder="Votre prenom"
+        keyboardType="default"
+      />
+      <TextInput
+        style={styles.textInput}
+        onChangeText={newText => {}}
+        defaultValue={""}
+        placeholder="password"
+        keyboardType="default"
+        secureTextEntry
+      />
         <Button title="Connect" onPress={() => {
           console.log("Coucouuu");
           navigation.navigate('Home')
@@ -18,14 +34,22 @@ const LoginScreen = ({ navigation }: LoginProps) => {
 
   const styles = StyleSheet.create({
     mainContainer: {
-      alignItems: "center",
+      alignItems: "stretch",
       justifyContent: "center",
       backgroundColor: "white",
       height: "100%",
+      flexDirection: 'column',
+      padding: 50,
     },
     loginscreen: {
-      fontSize: 50,
+      fontSize: 40,
       fontWeight: "bold",
+    },
+    textInput: {
+      borderWidth: 1,
+      borderRadius: 5,
+      marginTop: 5,
+      marginBottom: 5
     },
   });
   export default LoginScreen;
